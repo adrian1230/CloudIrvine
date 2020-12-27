4cbcc8d34e3c25a745ea88d8d7805d6938a2dfb4
adrian1230 <adrian1145d98@gmail.com>
2020-12-28 00:38:06 +0800

    adrian

--- screens/Ranking.js
+++ screens/Ranking.js
@@ -6,19 +6,52 @@ import {
     View,
 } from "react-native";
 
-const LD = () => {
-    return (
-        <View style={{width:11,paddingVertical:15}}>
-            <View style={{flex:2,borderRightColor:'white',borderRightWidth:10}}></View>
-        </View>
-    )
-}
-
 const Ranking = () => {
+    const texts_ = [
+        {"title":"Model Syrion"},
+        {"title":"Model Pomme"},
+        {"title":"Model Daemon"},
+        {"title":"Model Alexander"}
+    ];
+    
+    var d = [];
+
+    while (d.length < 4) {
+        var dd = Math.floor(Math.random()*4);
+        if (d.includes(dd)===false) {
+            d.push(dd);
+        } 
+    }
+
+    var ttt = [];
+
+    while (ttt.length < 4) {
+        for (var i =0; i < d.length;i++) {
+            ttt.push(texts_[d[i]])
+        }
+    }
+
     return (
         <SafeAreaView style={styles.body}>
             <Text style={styles.text_h1}>Ranking</Text>
             <View style={{borderWidth:0.5,borderColor:'white',margin:10}}></View>
+            <View style={styles.container_101}>
+                {
+                    ttt.map(
+                        (p) =>
+                            <View 
+                                style={
+                                    {
+                                        marginHorizontal:15,
+                                        alignSelf: 'center'
+                                    }
+                                }
+                            >
+                                <Text style={{color:'red'}}>{p.title}</Text>
+                            </View>
+                    )
+                }
+            </View>
         </SafeAreaView>
     )
 }
@@ -26,6 +59,11 @@ const Ranking = () => {
 const styles = StyleSheet.create({
     body: {
         backgroundColor: "black"
+    },
+    container_101: {
+        height: '100%',
+        width: '100%',
+        backgroundColor: 'black'
     },
     text_h1: {
         color: "white",
