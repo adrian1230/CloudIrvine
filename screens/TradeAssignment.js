614e6c73fc1e1ddfcdbf9878ce1f112aa9f791c5
adrian1230 <adrian1145d98@gmail.com>
2020-12-28 02:34:39 +0800

    adrian

--- screens/TradeAssignment.js
+++ screens/TradeAssignment.js
@@ -2,14 +2,69 @@ import React from "react";
 import {
     Text,
     View,
+    StyleSheet,
+    Image,
+    SafeAreaView,
+    ScrollView,
 } from "react-native";
 
 const OrderTrade = () => {
+    const selection_ = [
+        {
+            "title":"Model Syrion",
+            "img":require('../assets/images/fighter1.png'),
+            "pilots": ["Connor Doe","Bain Bain","Deak Jue","Daek D"]
+        },
+        {
+            "title":"Model Pomme",
+            "img":require('../assets/images/fighter2.png'),
+            "pilots": ["Ensku 8"]
+        },
+        {
+            "title":"Model Daemon",
+            "img":require('../assets/images/fighter3.png'),
+            "pilots": ["Plasima Fuku","Bob Coku"]
+        },
+        {
+            "title":"Model Alexander",
+            "img":require('../assets/images/fighter4.png'),
+            "pilots": ["Perry Jo","Fiona Jo"]
+        }
+    ];
+
     return (
-        <View>
-            <Text>OrderTrade</Text>
-        </View>
+        <SafeAreaView>
+            <ScrollView>
+                <View style={styles.container_102}>
+                    <View>
+                        <Text style={{
+                            textAlign:"center",
+                            fontStyle:'italic',
+                            fontWeight:'700',
+                            color:'red',
+                            fontSize:30}}
+                        >
+                            Order
+                        </Text>
+                        <View style={{
+                                width:"90%",
+                                marginTop: 15,
+                                margin: 20,
+                                borderColor: 'grey',
+                                borderWidth: 1.5
+                            }}
+                        ></View>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container_102: {
+        marginTop: 20
+    }
+})
 
 export default OrderTrade;
