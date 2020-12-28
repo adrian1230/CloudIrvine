5395a37f00a9de2fcb7143ce30f23f0feb6c0258
adrian1230 <adrian1145d98@gmail.com>
2020-12-28 23:33:02 +0800

    adrian

--- screens/ParkingLot.js
+++ screens/ParkingLot.js
@@ -2,7 +2,13 @@ import React from "react";
 import {
     Text,
     View,
+    Image,
+    TouchableOpacity,
 } from "react-native";
+import MapView, {PROVIDER_GOOGLE,Marker} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+
+import {gakey} from "../consts/index";
 
 const ParkingLot = () => {
     return (
