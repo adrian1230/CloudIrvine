bf4d3227751dd2649012cd45baffea57db849068
adrian1230 <adrian1145d98@gmail.com>
2020-12-30 02:20:24 +0800

    adrian

--- screens/ParkingLot.js
+++ screens/ParkingLot.js
@@ -252,49 +252,6 @@ const ParkingLot = ({route}) => {
         )
     }
 
-    function renderDestinationHeader() {
-        return (
-            <View
-                style={{
-                    position: 'absolute',
-                    top: 50,
-                    left: 0,
-                    right: 0,
-                    height: 50,
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}
-            >
-                <View
-                    style={{
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        width: SIZES_.width * 0.9,
-                        paddingVertical: SIZES_.padding,
-                        paddingHorizontal: SIZES_.padding * 2,
-                        borderRadius: SIZES_.radius,
-                        backgroundColor: 'white'
-                    }}
-                >
-                    <Image
-                        source={icons.map_}
-                        style={{
-                            width: 30,
-                            height: 30,
-                            marginRight: SIZES_.padding
-                        }}
-                    />
-
-                    <View style={{ flex: 1 }}>
-                        <Text style={{ ...FONTS_.body3 }}>{streetName}</Text>
-                    </View>
-
-                    <Text style={{ ...FONTS_.body3 }}>{Math.ceil(duration)} mins</Text>
-                </View>
-            </View>
-        )
-    }
-
     function renderButtons() {
         return (
             <View
@@ -342,7 +299,6 @@ const ParkingLot = ({route}) => {
     return (
         <View style={{ flex: 1 }}>
             {renderMap()}
-            {renderDestinationHeader()}
             {renderButtons()}
         </View>
     )
